c4b15edf2d5b8b878c0339fdbf300d6f20175f2b
Xi Zhang <13602037587@163.com>
2021-10-08 12:55:39 -0300

    Add files via upload

---
+++ coarse_tuning/scripts/TEST.sh
@@ -0,0 +1,11 @@
+# TreeTrimmer
+# 
+# ver.130412
+# Maruyama et al. TreeTrimmer: a method for phylogenetic dataset size reduction. BMC Research Notes 2013, 6:145.
+# written by Shinichiro Maruyama / Robert J. M. Eveleigh / John M. Archibald
+
+# Fig. 1b
+ruby treetrimmer.rb sample/sample_tree.tre sample/sample_paramer_input1.in sample/sample_taxonomic_info.txt > out1
+
+# Fig. 1c
+ruby treetrimmer.rb sample/sample_tree.tre sample/sample_paramer_input2.in sample/sample_taxonomic_info.txt > out2

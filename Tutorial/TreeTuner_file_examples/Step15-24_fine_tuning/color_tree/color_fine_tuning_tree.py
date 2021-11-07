9d329c0a97f9a92a9f612bc7e696d4ee915f5222
Xi Zhang <13602037587@163.com>
2021-11-07 19:31:25 -0400

    Update color_fine_tuning_tree.py

--- Tutorial/TreeTuner_file_examples/Step15-24_fine_tuning/color_tree/color_fine_tuning_tree.py
+++ Tutorial/TreeTuner_file_examples/Step15-24_fine_tuning/color_tree/color_fine_tuning_tree.py
@@ -1,3 +1,21 @@
+# TreeTuner v1.0
+
+# Copyright 2021 Zhang X., Hu Y., Eme L., Maruyama S.,Eveleigh R.JM, Curtis B.A., Sibbald S.J., Hopkins J.F., Filloramo1 G.V.,Wijk K.J.V., Archibald J.M.
+
+
+#   This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from ete3 import Tree, TreeStyle, TextFace, NodeStyle
 
 import sys
